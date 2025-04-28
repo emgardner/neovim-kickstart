2f3cdf5cd890d1524accc9f611fad0be692bfc51
emgardner <gardner.ethan10@gmail.com>
2025-04-28 07:40:42 -0400

    Added Language Server Autoformat

--- init.lua
+++ init.lua
@@ -591,7 +591,11 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          procMacro = {
+            enable = true,
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -675,6 +679,11 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        tsx = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        javascript = { 'prettier' },
+        json = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
